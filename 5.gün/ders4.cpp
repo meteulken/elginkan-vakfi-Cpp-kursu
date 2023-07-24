1fd65e7b800cb8181d0dcd5051bd0608c37ceb85
Mete <mete.ulken71@gmail.com>
2023-07-24 12:04:13 +0300

    added

---
+++ 5.gün/ders4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int s1 = 0;
+    int s2 = 0;
+
+    cout << "Satir giriniz : ";
+    cin >> s1;
+    int hesap = s1* 2 - 1;
+    for (size_t i = 0; i < s1; i++)
+    {
+        for (size_t j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (size_t i = 0; i < count; i++)
+        {
+            setw(hesap);
+        }
+        
+        
+        cout << endl; 
+    }
+  
+}
---
+++ 5.gün/ders5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int s1 = 0;
+    int s2 = 0;
+
+    
+}
