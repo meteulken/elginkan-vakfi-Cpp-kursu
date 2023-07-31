71f7a9956903bdc7dfdedcbcd4e69484c5a5bbed
Mete <mete.ulken71@gmail.com>
2023-07-31 14:14:17 +0300

    fix

--- odevs4/exercise1.cpp
+++ odevs4/exercise1.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
 using namespace std;
 
+// Fonksiyonlar
 int varmi(int dizi[], int boyut, int aranan)
 {
-    int i = 0;
-    for (i = 0; i < boyut; i++)
+    for (int i = 0; i < boyut; i++)
     {
         if (dizi[i] == aranan)
         {
-            return 1;
+            return 1; // Eşleşen sayı var
         }
     }
-    return 0;
+    return 0; // Eşleşen sayı yok
 }
 
 int main()
@@ -23,73 +22,73 @@ int main()
     int rndS2 = 0;
     int rndS3 = 0;
     int rndS4 = 0;
-    int thmnS1 = 0;
-    int thmnS2 = 0;
-    int thmnS3 = 0;
-    int thmnS4 = 0;
-    int randomSayi = 0;
     int dizi[4];
     int tahmin;
     int thmnDizi[4];
     int hak = 0;
+    bool sayiBulundu = false; // Sayı bulunduğunda true olacak değişken
 
     srand(time(NULL));
-
-    // rndS1 = (rand() % 10) * 1000;
-    // rndS2 = (rand() % 100) * 10;
-    // rndS3 = rand() % 100;
 
     rndS1 = (rand() % 10);
     rndS2 = (rand() % 10);
     rndS3 = (rand() % 10);
     rndS4 = (rand() % 10);
 
-
-    cout << rndS1 << rndS2 << rndS3 << rndS4 <<  endl;
-
-    randomSayi = rndS1+ rndS2 + rndS3;
-
     dizi[0] = rndS1;
     dizi[1] = rndS2;
     dizi[2] = rndS3;
     dizi[3] = rndS4;
 
+    cout << rndS1 << rndS2 << rndS3 << rndS4 << endl;
 
-
-    cout << "Tahmininizi giriniz: ";
-    cin >> tahmin;
-
-    thmnS1 = tahmin / 1000;
-    thmnS2 = tahmin / 100 % 10;
-    thmnS3 = tahmin / 10 % 10;
-    thmnS4 = tahmin % 10;
-
-    cout << thmnS1 << endl;
-    cout << thmnS2 << endl;
-    cout << thmnS3 << endl;
-    cout << thmnS4 << endl;
-    
-    while (hak < 10)
+    while (hak < 10 && !sayiBulundu)
     {
-        thmnDizi[0] = thmnS1;
-        thmnDizi[1] = thmnS2;
-        thmnDizi[2] = thmnS3;
-        thmnDizi[3] = thmnS4;
-        int i = 0;
-        int j = 0;
+        cout << "Tahmininizi giriniz: ";
+        cin >> tahmin;
 
-        while(i < 4)
+        thmnDizi[0] = tahmin / 1000;
+        thmnDizi[1] = tahmin / 100 % 10;
+        thmnDizi[2] = tahmin / 10 % 10;
+        thmnDizi[3] = tahmin % 10;
+
+        int dogruYer = 0;
+        int yanlisYer = 0;
+
+        for (int i = 0; i < 4; i++)
         {
-            if(varmi(dizi[i], 4, thmnS1))
+            if (dizi[i] == thmnDizi[i])
             {
-                cout << "Eslesen sayi var : " << dizi[i] << endl;
+                dogruYer++;
             }
-            else
+            else if (varmi(dizi, 4, thmnDizi[i]))
             {
-                cout << "Eslesen sayi var : " << dizi[i] << endl;
+                yanlisYer++;
             }
-            i++;
         }
+
+        cout << "-> -" << yanlisYer << " , +" << dogruYer << endl;
+
+        if (dogruYer == 4)
+        {
+            sayiBulundu = true;
+        }
+
         hak++;
     }
+
+    // Oyun bittiğinde sonucu göster
+    if (sayiBulundu)
+    {
+        cout << "Tebrikler, sayiyi buldunuz!" << endl;
+        int puan = 10 - hak + 1;
+        cout << "Puaniniz: " << puan << endl;
+    }
+    else
+    {
+        cout << "Maalesef, sayiyi bulamadiniz." << endl;
+        cout << "Puaniniz: 0" << endl;
+    }
+
+    return 0;
 }
