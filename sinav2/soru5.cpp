9ab9b89f27b950c2a774bf7f81c60b34f46e818e
Mete <mete.ulken71@gmail.com>
2023-07-31 13:12:57 +0300

    sinav finish

--- sinav2/soru5.cpp
+++ sinav2/soru5.cpp
@@ -2,60 +2,160 @@
 
 using namespace std;
 
-
-string sayiDonustur(int sayi)
-{
-    switch (sayi)
-    {
-    case 1:
-        return "bir";
-        break;
-    case 2:
-        return "iki";
-        break;
-    case 3:
-        return "uc";
-        break;
-    case 4:
-        return "dort";
-        break;
-    case 5:
-        return "bes";
-        break;
-    case 6:
-        return "alti";
-        break;
-    case 7:
-        return "yedi";
-        break;
-    case 8: 
-        return "sekiz";
-        break;
-    case 9:
-        return "dokuz";
-        break;
-}
-
+string sayiDonustur(int sayi);
 
 int main()
 {
-    int not1 = 0, not2 = 0, not3 = 0;
-    int input = 0;
-    int ort = 0;
+
+    int not1, not2, not3, ortalama;
 
     do
-    {   
+    {
         cout << "1. notu giriniz: ";
-        cin >> not;
+        cin >> not1;
         cout << "2. notu giriniz: ";
         cin >> not2;
         cout << "3. notu giriniz: ";
         cin >> not3;
-        ort = (not + not2 + not3) / 3;
 
+        ortalama = (not1 + not2 + not3) / 3;
 
+        if (not1 < 0 || not1 > 100 || not2 < 0 || not2 > 100 || not3 < 0 || not3 > 100)
+        {
+            cout << "Hatali not girisi yaptiniz. Lutfen tekrar deneyiniz." << endl;
+        }
 
-    } while (not < 0 || not > 100 || not2 < 0 || not2 > 100 || not3 < 0 || not3 > 100);
+        else if (ortalama >= 50)
+        {
+            cout << "Ortalamaniz: " << ortalama <<" " << sayiDonustur(ortalama) << " Gectiniz." << endl;
+        }
+
+        else if (ortalama < 50)
+        {
+            cout << "Ortalamaniz: " << ortalama << " "<< sayiDonustur(ortalama) << " Kaldiniz." << endl;
+        }
+
+    }while(not1 < 0 || not1 > 100 || not2 < 0 || not2 > 100 || not3 < 0 || not3 > 100);
     
+}
 
+
+string sayiDonustur(int sayi)
+{
+    string yazi;
+
+    if(sayi >= 1 && sayi <= 999)
+    {
+        switch (sayi / 100)
+        {
+        {
+        case 0:
+            yazi = " ";
+            break;
+        case 1:
+            yazi = "yuz";
+            break;
+        case 2:
+            yazi = "ikiyuz";
+            break;
+        case 3:
+            yazi = "ucyuz";
+            break;
+        case 4:
+            yazi = "dortyuz";
+            break;
+        case 5:
+            yazi = "besyuz";
+            break;
+        case 6:
+            yazi = "altiyuz";
+            break;
+        case 7:
+            yazi = "yediyuz";
+            break;
+        case 8:
+            yazi = "sekizyuz";
+            break;
+        case 9:
+            yazi = "dokuzyuz";
+            break;
+        }            
+        default:
+            break;
+        }
+        switch (sayi/10)
+        {
+        {
+        case 0:
+            yazi += " ";
+            break;
+        case 1:
+            yazi += "on";
+            break;
+        case 2:
+            yazi += "yirmi";
+            break;
+        case 3:
+            yazi += "otuz";
+            break;
+        case 4:
+            yazi += "kirk";
+            break;
+        case 5:
+            yazi += "elli";
+            break;
+        case 6:
+            yazi += "altmis";
+            break;
+        case 7:
+            yazi += "yetmis";
+            break;
+        case 8:
+            yazi += "seksen";
+            break;
+        case 9:
+            yazi += "doksan";
+            break;
+        }
+        default:
+            break;
+        }
+        switch (sayi % 10)
+        {
+        {
+            case 0:
+                yazi += " ";
+                break;
+            case 1:
+                yazi += "bir";
+                break;
+            case 2:
+                yazi += "iki";
+                break;
+            case 3:
+                yazi += "üc";
+                break;
+            case 4:
+                yazi += "dort";
+                break;
+            case 5:
+                yazi += "bes";
+                break;
+            case 6:
+                yazi += "alti";
+                break;
+            case 7:
+                yazi += "yedi";
+                break;
+            case 8:
+                yazi += "sekiz";
+                break;
+            case 9:
+                yazi += "dokuz";
+                break;
+        }
+        }
+        return yazi;
+    }
+    return " ";     
 }
